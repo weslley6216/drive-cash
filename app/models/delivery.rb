c93c0d5ee562bd37e6d836a0bd8741e53c535aa7
Weslley Campos <weslley.campos@rebase.com.br>
2026-01-15 18:14:17 -0300

    feat: add delivery model

---
+++ app/models/delivery.rb
@@ -0,0 +1,18 @@
+class Delivery < ApplicationRecord
+    validates :date, presence: true
+    validates :route_value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+    validates :fuel_cost,
+              :maintenance_cost,
+              :other_costs,
+              numericality: { greater_than_or_equal_to: 0 }
+  
+    def total_costs
+      fuel_cost + maintenance_cost + other_costs
+    end
+  
+    def net_profit
+      route_value - total_costs
+    end
+  end
+  
