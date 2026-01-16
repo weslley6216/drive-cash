219fe75b5cd19b1915039009db4181c206fa7b85
Weslley Campos <weslley.campos@rebase.com.br>
2026-01-16 15:36:28 -0300

    feat: add Tailwind CSS

---
+++ config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/views/**/*.{erb,haml,html,slim,rb}',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+export TAILWIND_DISABLE_WATCHMAN=1
+
 if [ "$(id -u)" = "0" ]; then
-	mkdir -p /usr/local/bundle /app/tmp/pids
-	chown -R appuser:appuser /usr/local/bundle /app
-	su -s /bin/bash -c 'rm -f tmp/pids/server.pid' appuser
-	exec su -s /bin/bash -c "$*" appuser
+  mkdir -p /usr/local/bundle /app/tmp/pids
+  chown -R appuser:appuser /usr/local/bundle /app
+
+  echo "🎨 Building Tailwind CSS..."
+  su -s /bin/bash -c 'bundle exec rails tailwindcss:build' appuser
+
+  su -s /bin/bash -c 'rm -f tmp/pids/server.pid' appuser
+  exec su -s /bin/bash -c "$*" appuser
 fi
 
 rm -f tmp/pids/server.pid
