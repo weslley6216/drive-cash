e936422be8a909081c243ef6ce99947a02c8fabc
Weslley Campos <weslley.campos@rebase.com.br>
2026-01-16 23:35:44 -0300

    chore: move phlex components to app root and configure zeitwerk autoloading

---
+++ app/views/concerns/formatting.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Formatting
+  include ActionView::Helpers::NumberHelper
+
+  def format_currency(value)
+    number_to_currency(value, unit: 'R$ ', separator: ',', delimiter: '.', precision: 2)
+  end
+
+  def format_percentage(value)
+    number_with_precision(value, precision: 1)
+  end
+
+  def format_decimal(value)
+    number_with_precision(value, precision: 1)
+  end
+end
