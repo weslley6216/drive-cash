da8efe456d483326002f28d16c8b5dc049ec3041
Weslley Campos <weslley.camphos@hotmail.com>
2026-01-18 10:45:33 -0300

    feat: add i18n setup

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < Phlex::HTML
+  # Rails View Helpers
   include Phlex::Rails::Helpers::Routes
   include Phlex::Rails::Helpers::DOMID
+
+  # Rails I18n Helpers
+  include Phlex::Rails::Helpers::T
+  include Phlex::Rails::Helpers::L
+
+  # Custom Helpers
   include Formatting
 
   def class_names(*classes)
