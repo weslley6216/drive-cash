d8d2bbb0f38fe96ab642567bc36c1e263d334898
Weslley Campos <weslley.campos@rebase.com.br>
2026-01-22 17:20:43 -0300

    chore: add solid cache table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_131302) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_22_201715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_22_131302) do
     t.index ["category"], name: "index_expenses_on_category"
     t.index ["date"], name: "index_expenses_on_date"
     t.index ["trip_id"], name: "index_expenses_on_trip_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
+    t.datetime "created_at", null: false
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["created_at"], name: "index_solid_cache_entries_on_created_at"
+    t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "trips", force: :cascade do |t|
