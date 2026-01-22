9119bb47aa0148e048f2eee42d24e854575f0c69
Weslley Campos <weslley.campos@rebase.com.br>
2026-01-22 17:18:51 -0300

    chore: add solid cache table

---
+++ db/migrate/20260122201715_create_solid_cache_entries.rb
@@ -0,0 +1,11 @@
+class CreateSolidCacheEntries < ActiveRecord::Migration[7.1]
+  def change
+    create_table :solid_cache_entries, if_not_exists: true do |t|
+      t.binary   :key,        null: false,   limit: 1024, index: { unique: true, length: 1024 }
+      t.binary   :value,      null: false,   limit: 536870912
+      t.datetime :created_at, null: false,   index: true
+      t.integer  :key_hash,   null: false,   limit: 8,    index: true
+      t.integer  :byte_size,  null: false,   limit: 4
+    end
+  end
+end
