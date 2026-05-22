8cab32114e003070ac48306b2b2b1249eb750476
Weslley Campos <weslley.camphos@gmail.com>
2026-05-22 20:16:01 -0300

    feat: add Application::ComingSoonView and placeholder routes spec

---
+++ spec/requests/coming_soon_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Coming soon placeholders', type: :request do
+  %w[/analysis /work_session /history /settings].each do |path|
+    describe "GET #{path}" do
+      it 'responds 200 with em breve content' do
+        get path
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include(I18n.t('application.coming_soon_view.title'))
+        expect(response.body).to include(I18n.t('application.coming_soon_view.back'))
+        expect(response.body).to include('href="/"')
+      end
+    end
+  end
+end
