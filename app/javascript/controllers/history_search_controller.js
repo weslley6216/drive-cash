d1f06257a8598338eab07c83b51ab4c31400cee8
Weslley Campos <weslley.camphos@gmail.com>
2026-05-30 22:55:32 -0300

    feat(history): add Stimulus controller for search debounce auto-submit

---
+++ app/javascript/controllers/history_search_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { wait: { type: Number, default: 300 } }
+
+  connect() {
+    this.timeout = null
+  }
+
+  disconnect() {
+    if (this.timeout) clearTimeout(this.timeout)
+  }
+
+  debounce() {
+    if (this.timeout) clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => this.element.requestSubmit(), this.waitValue)
+  }
+}
