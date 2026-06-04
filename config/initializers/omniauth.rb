23b379985a21b1646212490e438b46a279dd9859
Weslley Campos <weslley.camphos@hotmail.com>
2026-06-03 21:17:29 -0300

    fix: guard omniauth provider registration when credentials absent

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           ENV['GOOGLE_CLIENT_ID'],
-           ENV['GOOGLE_CLIENT_SECRET'],
-           scope: 'email,profile'
+  if ENV['GOOGLE_CLIENT_ID'].present? && ENV['GOOGLE_CLIENT_SECRET'].present?
+    provider :google_oauth2,
+             ENV['GOOGLE_CLIENT_ID'],
+             ENV['GOOGLE_CLIENT_SECRET'],
+             scope: 'email,profile'
+  end
 end
 
 OmniAuth.config.on_failure = proc do |env|
