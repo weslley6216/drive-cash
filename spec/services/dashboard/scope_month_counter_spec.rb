accaaf8b41131b1b9da1db514bafe0931941a4a0
Weslley Campos <weslley.camphos@hotmail.com>
2026-06-06 11:34:16 -0300

    test: add characterization spec for ScopeMonthCounter.count_for

---
+++ spec/services/dashboard/scope_month_counter_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Dashboard::ScopeMonthCounter do
+  describe '.count_for' do
+    it 'returns 1 for an empty scope (clamp floor)' do
+      result = described_class.count_for(Earning.none)
+
+      expect(result).to eq(1)
+    end
+
+    it 'returns 1 when all records are in the same month' do
+      create(:earning, date: Date.new(2025, 3, 1))
+      create(:earning, date: Date.new(2025, 3, 15))
+
+      result = described_class.count_for(Earning.all)
+
+      expect(result).to eq(1)
+    end
+
+    it 'returns distinct month count when records span multiple months' do
+      create(:earning, date: Date.new(2025, 1, 10))
+      create(:earning, date: Date.new(2025, 3, 5))
+      create(:earning, date: Date.new(2025, 3, 20))
+      create(:earning, date: Date.new(2025, 7, 1))
+
+      result = described_class.count_for(Earning.all)
+
+      expect(result).to eq(3)
+    end
+  end
+end
