27addcb731a5996a2a64bc0edfcd98cad2c05583
Weslley Campos <weslley.camphos@hotmail.com>
2026-06-07 16:00:43 -0300

    refactor(expenses): require explicit user kwarg in Creator

--- spec/services/expenses/creator_spec.rb
+++ spec/services/expenses/creator_spec.rb
@@ -10,24 +10,31 @@ RSpec.describe Expenses::Creator do
           date: '2026-01-10',
           amount: 100.00,
           category: 'fuel',
-          vendor: 'Shell',
-          user_id: user.id
+          vendor: 'Shell'
         }
       end
 
-      it 'creates a single paid expense' do
-        result = described_class.call(expense_params, {})
+      it 'creates a single paid expense owned by the user' do
+        result = described_class.call(expense_params, {}, user: user)
 
         expect(result.success?).to be true
         expect(result.expenses.size).to eq(1)
         expect(result.expenses.first.paid).to be true
+        expect(result.expenses.first.user).to eq(user)
       end
 
       it 'returns failure when expense is invalid' do
-        result = described_class.call({ amount: -10 }, {})
+        result = described_class.call({ amount: -10 }, {}, user: user)
 
         expect(result.success?).to be false
         expect(result.expense.errors).to be_present
+      end
+
+      it 'ignores user_id forged inside the attributes payload' do
+        other = create(:user)
+        result = described_class.call(expense_params.merge(user_id: other.id), {}, user: user)
+
+        expect(result.expenses.first.user).to eq(user)
       end
     end
 
@@ -37,8 +44,7 @@ RSpec.describe Expenses::Creator do
           date: '2026-01-10',
           amount: 300.00,
           category: 'maintenance',
-          vendor: 'Oficina',
-          user_id: user.id
+          vendor: 'Oficina'
         }
       end
 
@@ -46,11 +52,12 @@ RSpec.describe Expenses::Creator do
         { repeat: true, period: 'monthly', repetitions: 3 }
       end
 
-      it 'delegates to InstallmentCreator' do
-        result = described_class.call(expense_params, installment_params)
+      it 'delegates to InstallmentCreator passing the user along' do
+        result = described_class.call(expense_params, installment_params, user: user)
 
         expect(result.success?).to be true
         expect(result.expenses.size).to eq(3)
+        expect(result.expenses.map(&:user)).to all(eq(user))
       end
     end
   end
