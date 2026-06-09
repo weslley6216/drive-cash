84ca5f9c8d7359a6c0c2a40be8646e6a45de339c
Weslley Campos <weslley.camphos@hotmail.com>
2026-06-09 11:03:39 -0300

    feat(nav): add Metas tab to bottom nav with Target icon

--- spec/components/bottom_nav_component_spec.rb
+++ spec/components/bottom_nav_component_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe BottomNavComponent, type: :component do
   context 'when active: :home' do
     let(:active) { :home }
 
-    it 'renders 5 tabs with Lucide icons' do
-      expect(html.scan('<a ').size).to eq(5)
+    it 'renders 6 tabs with Lucide icons' do
+      expect(html.scan('<a ').size).to eq(6)
       expect(html).to include(I18n.t('bottom_nav_component.tabs.home'))
       expect(html).to include(I18n.t('bottom_nav_component.tabs.analysis'))
+      expect(html).to include(I18n.t('bottom_nav_component.tabs.goals'))
       expect(html).to include(I18n.t('bottom_nav_component.tabs.journey'))
       expect(html).to include(I18n.t('bottom_nav_component.tabs.history'))
       expect(html).to include(I18n.t('bottom_nav_component.tabs.more'))
@@ -30,15 +31,25 @@ RSpec.describe BottomNavComponent, type: :component do
     end
 
     it 'dims inactive tabs with text-slate-400' do
-      expect(html.scan('text-slate-400').size).to eq(4)
+      expect(html.scan('text-slate-400').size).to eq(5)
     end
 
     it 'links each tab to its corresponding route' do
       expect(html).to include('href="/"')
       expect(html).to include('href="/analysis"')
+      expect(html).to include('href="/goals"')
       expect(html).to include('href="/work_session"')
       expect(html).to include('href="/history"')
       expect(html).to include('href="/account"')
+    end
+  end
+
+  context 'when active: :goals' do
+    let(:active) { :goals }
+
+    it 'highlights only the goals tab' do
+      expect(html.scan('text-blue-600').size).to eq(1)
+      expect(html).to include('href="/goals"')
     end
   end
 
