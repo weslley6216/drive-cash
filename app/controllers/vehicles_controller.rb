216c65c3bea67c5abed5a681a2a37967dae10045
Weslley Campos <weslley.camphos@hotmail.com>
2026-06-22 18:21:25 -0300

    refactor(vehicles): inline dashboard_payload, empty_payload and odometer_changed? in actions

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,9 +2,12 @@ class VehiclesController < ApplicationController
   def show
     @vehicle = current_user.vehicle
     if @vehicle
-      render Vehicles::ShowView.new(payload: dashboard_payload)
+      payload = Vehicles::MaintenanceService.new(user: current_user).call
+        .merge(tank: Vehicles::TankBalanceService.new(user: current_user).call)
+      render Vehicles::ShowView.new(payload: payload)
     else
-      render Vehicles::ShowView.new(payload: empty_payload, vehicle_form: current_user.build_vehicle)
+      empty = Vehicles::MaintenanceService::EMPTY_PAYLOAD.merge(tank: Vehicles::TankBalanceService::EMPTY)
+      render Vehicles::ShowView.new(payload: empty, vehicle_form: current_user.build_vehicle)
     end
   end
 
@@ -15,31 +18,24 @@ class VehiclesController < ApplicationController
 
   def update
     @vehicle = current_user.vehicle || current_user.build_vehicle
-    attributes = odometer_changed? ? vehicle_params.merge(odometer_updated_at: Time.current) : vehicle_params
+    odometer_changed = vehicle_params[:odometer_km].present? && vehicle_params[:odometer_km].to_i != @vehicle.odometer_km
+    attributes = odometer_changed ? vehicle_params.merge(odometer_updated_at: Time.current) : vehicle_params
 
     if @vehicle.update(attributes)
       flash[:notice] = t('vehicle.flash.updated')
       respond_with_modal_refresh(html_redirect: vehicle_path)
     else
-      payload = current_user.vehicle ? dashboard_payload : empty_payload
+      payload = if current_user.vehicle
+                  Vehicles::MaintenanceService.new(user: current_user).call
+                    .merge(tank: Vehicles::TankBalanceService.new(user: current_user).call)
+      else
+                  Vehicles::MaintenanceService::EMPTY_PAYLOAD.merge(tank: Vehicles::TankBalanceService::EMPTY)
+      end
       render Vehicles::ShowView.new(payload: payload, vehicle_form: @vehicle), status: :unprocessable_content
     end
   end
 
   private
-
-  def dashboard_payload
-    Vehicles::MaintenanceService.new(user: current_user).call
-      .merge(tank: Vehicles::TankBalanceService.new(user: current_user).call)
-  end
-
-  def empty_payload
-    Vehicles::MaintenanceService::EMPTY_PAYLOAD.merge(tank: Vehicles::TankBalanceService::EMPTY)
-  end
-
-  def odometer_changed?
-    vehicle_params[:odometer_km].present? && vehicle_params[:odometer_km].to_i != @vehicle.odometer_km
-  end
 
   def vehicle_params
     params.require(:vehicle).permit(:brand, :vehicle_model, :year, :license_plate, :odometer_km)
