3fe2898c24a29250eda0867704b475eb9ff0c550
Weslley Campos <weslley.camphos@hotmail.com>
2026-07-07 16:12:39 -0300

    fix(monetary): treat comma as decimal separator regardless of digit count

--- app/models/concerns/monetary_amount.rb
+++ app/models/concerns/monetary_amount.rb
@@ -23,7 +23,7 @@ module MonetaryAmount
     integer_part = cleaned[0...last_separator].delete('.,')
     decimal_part = cleaned[(last_separator + 1)..]
 
-    if decimal_part.length == 3 && cleaned.count('.,') == 1
+    if cleaned[last_separator] == '.' && decimal_part.length == 3 && cleaned.count('.,') == 1
       "#{integer_part}#{decimal_part}"
     else
       "#{integer_part}.#{decimal_part}"
