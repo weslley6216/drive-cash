4c46a04e370677473975e7befe9fac1d1ff55d9d
Weslley Campos <weslley.camphos@hotmail.com>
2026-07-09 14:41:29 -0300

    refactor: extract a single refresh_stream turbo helper

--- app/views/concerns/turbo_update_response.rb
+++ app/views/concerns/turbo_update_response.rb
@@ -7,7 +7,7 @@ module TurboUpdateResponse
     if record.persisted? && @totals
       modal_stream(detail_view.new(**detail, filters: @context))
       flash_stream('flash_modal', inline: true)
-      raw '<turbo-stream action="refresh"></turbo-stream>'.html_safe
+      refresh_stream
     else
       modal_stream(edit_view.new(record_key => record, context: @context))
       flash_stream
