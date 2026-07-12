9d89a09a3b228f6167f43958dea5ee75655910fd
Weslley Campos <weslley.camphos@hotmail.com>
2026-07-12 01:16:37 -0300

    refactor(routes): remove work_session and settings dead-end routes

--- spec/requests/coming_soon_spec.rb
+++ spec/requests/coming_soon_spec.rb
@@ -1,27 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'Coming soon placeholders', type: :request do
+RSpec.describe 'Coming soon placeholder', type: :request do
   let(:current_user) { create(:user) }
 
   before { login_as(current_user) }
 
-  %w[/work_session /settings].each do |path|
-    describe "GET #{path}" do
-      it 'responds 200 with em breve content' do
-        get path
+  describe 'GET /coming_soon' do
+    it 'responds 200 with em breve content' do
+      get '/coming_soon'
 
-        expect(response).to have_http_status(:success)
-        expect(response.body).to include(I18n.t('application.coming_soon_view.title'))
-        expect(response.body).to include(I18n.t('application.coming_soon_view.back'))
-        expect(response.body).to include('href="/"')
-      end
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include(I18n.t('application.coming_soon_view.title'))
+      expect(response.body).to include(I18n.t('application.coming_soon_view.back'))
+      expect(response.body).to include('href="/"')
+    end
 
-      it 'renders bottom nav so users can navigate away' do
-        get path
+    it 'renders bottom nav so users can navigate away' do
+      get '/coming_soon'
 
-        expect(response.body).to include('fixed bottom-0 left-0 right-0')
-        expect(response.body).to include(I18n.t('bottom_nav_component.tabs.home'))
-      end
+      expect(response.body).to include('fixed bottom-0 left-0 right-0')
+      expect(response.body).to include(I18n.t('bottom_nav_component.tabs.home'))
     end
   end
 end
