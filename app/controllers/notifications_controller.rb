dff481e2748b77f55f3064829f65790edfcf9ba2
Weslley Campos <weslley.camphos@hotmail.com>
2026-07-16 04:32:57 -0300

    feat(notifications): add notification center with read and read_all

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,29 @@
+class NotificationsController < ApplicationController
+  before_action :find_notification, only: :read
+
+  def index
+    Notifications::Sweeper.new(user: current_user).call
+    render Notifications::IndexView.new(
+      groups:       Notifications::Grouping.new(current_user.notifications.chronological).call,
+      unread_count: current_user.notifications.unread.count
+    )
+  end
+
+  def read
+    @notification.mark_read!
+    respond_with_refresh(html_redirect: notifications_path)
+  end
+
+  def read_all
+    current_user.notifications.unread.mark_all_read!
+    respond_with_refresh(html_redirect: notifications_path)
+  end
+
+  private
+
+  def find_notification
+    @notification = current_user.notifications.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  end
+end
--- app/views/application_view.rb
+++ app/views/application_view.rb
@@ -1,6 +1,7 @@
 class ApplicationView < ApplicationComponent
   include Phlex::Rails::Helpers::TurboFrameTag
   include Phlex::Rails::Helpers::FormWith
+  include Phlex::Rails::Helpers::ButtonTo
   include ModalHeader
   include ModalStyles
   include FormFields
