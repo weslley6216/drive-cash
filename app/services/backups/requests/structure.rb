62d7bd848eba515293c86174bb1e3a9701355db3
Weslley Campos <weslley.camphos@hotmail.com>
2026-07-28 19:31:48 -0300

    feat(backups): guard sheet, banding and chart creation by existing state

---
+++ app/services/backups/requests/structure.rb
@@ -0,0 +1,102 @@
+module Backups
+  module Requests
+    class Structure
+      State = Data.define(:sheet_ids, :banded_titles, :charted_titles)
+
+      SUMMARY_PROFIT_COLUMN = 3
+      CHART_ANCHOR_COLUMN = 7
+      HEADER_COLOR = { red: 0.92, green: 0.94, blue: 0.96 }.freeze
+      FIRST_BAND_COLOR = { red: 1.0, green: 1.0, blue: 1.0 }.freeze
+      SECOND_BAND_COLOR = { red: 0.97, green: 0.98, blue: 0.99 }.freeze
+
+      def initialize(state:, summary_month_count:)
+        @state = state
+        @summary_month_count = summary_month_count
+      end
+
+      def missing_tabs
+        Tabs::ALL.reject { |tab| @state.sheet_ids.key?(tab.title) }.map { |tab| add_sheet(tab) }
+      end
+
+      def decorations
+        Tabs::ALL.filter_map { |tab| banding_for(tab) } + [chart].compact
+      end
+
+      private
+
+      def add_sheet(tab)
+        Google::Apis::SheetsV4::Request.new(
+          add_sheet: Google::Apis::SheetsV4::AddSheetRequest.new(
+            properties: Google::Apis::SheetsV4::SheetProperties.new(title: tab.title)
+          )
+        )
+      end
+
+      def banding_for(tab)
+        sheet_id = @state.sheet_ids[tab.title]
+        return nil if sheet_id.nil? || @state.banded_titles.include?(tab.title)
+
+        Google::Apis::SheetsV4::Request.new(
+          add_banding: Google::Apis::SheetsV4::AddBandingRequest.new(
+            banded_range: Google::Apis::SheetsV4::BandedRange.new(
+              range:          Google::Apis::SheetsV4::GridRange.new(sheet_id: sheet_id, start_column_index: 0, end_column_index: tab.width),
+              row_properties: Google::Apis::SheetsV4::BandingProperties.new(
+                header_color:      Google::Apis::SheetsV4::Color.new(**HEADER_COLOR),
+                first_band_color:  Google::Apis::SheetsV4::Color.new(**FIRST_BAND_COLOR),
+                second_band_color: Google::Apis::SheetsV4::Color.new(**SECOND_BAND_COLOR)
+              )
+            )
+          )
+        )
+      end
+
+      def chart
+        tab = Tabs.find(:summary)
+        sheet_id = @state.sheet_ids[tab.title]
+        return nil if sheet_id.nil? || @state.charted_titles.include?(tab.title) || @summary_month_count.zero?
+
+        Google::Apis::SheetsV4::Request.new(
+          add_chart: Google::Apis::SheetsV4::AddChartRequest.new(
+            chart: Google::Apis::SheetsV4::EmbeddedChart.new(spec: chart_spec(sheet_id), position: chart_position(sheet_id))
+          )
+        )
+      end
+
+      def chart_spec(sheet_id)
+        Google::Apis::SheetsV4::ChartSpec.new(
+          title:       I18n.t('backups.summary.chart_title'),
+          basic_chart: Google::Apis::SheetsV4::BasicChartSpec.new(
+            chart_type:      'COLUMN',
+            legend_position: 'BOTTOM_LEGEND',
+            domains:         [Google::Apis::SheetsV4::BasicChartDomain.new(domain: chart_data(sheet_id, 0))],
+            series:          [Google::Apis::SheetsV4::BasicChartSeries.new(series: chart_data(sheet_id, SUMMARY_PROFIT_COLUMN), target_axis: 'LEFT_AXIS')]
+          )
+        )
+      end
+
+      def chart_data(sheet_id, column)
+        Google::Apis::SheetsV4::ChartData.new(
+          source_range: Google::Apis::SheetsV4::ChartSourceRange.new(
+            sources: [
+              Google::Apis::SheetsV4::GridRange.new(
+                sheet_id:           sheet_id,
+                start_row_index:    1,
+                end_row_index:      @summary_month_count + 1,
+                start_column_index: column,
+                end_column_index:   column + 1
+              )
+            ]
+          )
+        )
+      end
+
+      def chart_position(sheet_id)
+        Google::Apis::SheetsV4::EmbeddedObjectPosition.new(
+          overlay_position: Google::Apis::SheetsV4::OverlayPosition.new(
+            anchor_cell: Google::Apis::SheetsV4::GridCoordinate.new(sheet_id: sheet_id, row_index: 1, column_index: CHART_ANCHOR_COLUMN)
+          )
+        )
+      end
+    end
+  end
+end
